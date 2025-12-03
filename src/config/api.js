dd39db62b383789d995b466c4b78ca47d078fcbf
fhammsnutribites-bit <fhammsnutribites@gmail.com>
2025-12-04 05:03:20 +0530

    Update API-2

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
 // API Configuration for Vite
-export const API_URL = import.meta.env.VITE_API_URL ?'https://fhamms.vercel.app': 'http://localhost:5000';
+export const API_URL = import.meta.env.VITE_API_URL ?'https://fhamms-service.vercel.app': 'http://localhost:5000';
 
